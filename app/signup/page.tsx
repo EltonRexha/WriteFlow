f90c50bea58cc6a36566308f947b12b679c0c662
Elton Rexha <elton_rexha@outlook.com>
2025-04-29 01:24:13 +0200

    project init

---
+++ app/signup/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const SignUpPage = () => {
+  return (
+    <div>
+      <h1>This is signup page</h1>
+      <button className="btn btn-xs btn-primary">This is my button</button>
+    </div>
+  );
+}
+
+export default SignUpPage
