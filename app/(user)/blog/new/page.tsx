c7fe5023e23265ab6da0ac0ed386d6eafd0e17b9
Elton Rexha <elton_rexha@outlook.com>
2025-05-09 11:20:24 +0200

    add text editor, create route to create new blogs

---
+++ app/(user)/blog/new/page.tsx
@@ -0,0 +1,12 @@
+import TextEditor from '@/app/components/textEditor/TextEditor';
+import React from 'react';
+
+const page = () => {
+  return (
+    <div className='lg:px-36'>
+      <TextEditor />
+    </div>
+  );
+};
+
+export default page;
---
+++ app/(user)/home/layout.tsx
@@ -0,0 +1,47 @@
+import { Limelight } from 'next/font/google';
+import Link from 'next/link';
+import clsx from 'clsx';
+import Avatar from '../../components/Avatar';
+
+const limeLight = Limelight({
+  weight: '400',
+});
+
+export default function UserLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div className="flex flex-col">
+      <nav className="md:px-36 bg-base-100">
+        <div className="navbar">
+          <div className="sm:navbar-start">
+            <Link
+              href="/home"
+              className={clsx(
+                'link link-hover text-xl sm:text-2xl font-bold',
+                limeLight.className
+              )}
+            >
+              WriteFlow
+            </Link>
+          </div>
+          <div className="flex ml-auto sm:navbar-end">
+            <ul className="flex gap-2 items-center px-1">
+              <li>
+                <Link href="/blog/new">
+                  <button className="btn btn-primary">Create</button>
+                </Link>
+              </li>
+              <li>
+                <Avatar />
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+      <div>{children}</div>
+    </div>
+  );
+}
